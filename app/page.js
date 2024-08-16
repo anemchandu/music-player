f54c6573553b661211e3363ab35be4b6305dce8c
Mani10101 <manikantanallamilli1234@gmail.com>
2024-08-16 22:14:54 +0530

    page.js file descriptive changed

--- app/page.js
+++ app/page.js
@@ -12,8 +12,8 @@ export default function Home() {
         <img src="/landing.png" className="md:h-screen w-full" />
       </div>
       <div className=" gap-4 mt-8 md:mt-0 flex md:h-screen md:w-1/2 flex-col justify-center items-center md:gap-[60px]">
-        <h1 className="text-[43px] font-bold font-dm">No More Limits</h1>
-        <h3 className="text-[25px] font-bold font-dm">Hassle Free Music</h3>
+        <h1 className="text-[43px] font-bold font-dm">Prem se Bolo Radhey Radhey</h1>
+        <h3 className="text-[25px] font-bold font-dm">Listen RadhaKrishna Songs in our playlist</h3>
         <Link href="/search">
           <button className="p-[3.5%] bg-black text-white md:p-[1.5%] rounded-lg">
             Click to get Started
